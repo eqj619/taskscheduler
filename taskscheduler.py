e7e827a9145a81905bb2c5ed90441e7d835c5e75
eqj619 <eqj619@gmail.com>
2020-03-30 22:19:51 -0700

    create list to task

--- taskscheduler.py
+++ taskscheduler.py
@@ -39,19 +39,37 @@ class Scheduler:
         numOfTasks = len(tasks)
         dp = [[0 for i in range(sprintBudget+1)] for j in range (numOfTasks+1)]
 
-        #print(self.numOfTask)
+        tdp = [[[Task(0,0,0) for k in range(numOfTasks+20)] for i in range(sprintBudget+1)] for j in range (numOfTasks+1)]
+
         for i in range (0, numOfTasks):
             #print(tasks[i].taskId)
             for w in range (1, sprintBudget+1):
                 #print(w)
                 if ( w >= tasks[i].storyPoint):
                     dp[i+1][w] = max (dp[i][w-tasks[i].storyPoint] + tasks[i].value, dp[i][w])
+                    if( dp[i+1][w] == dp[i][w]):
+                        for x in range(0, numOfTasks):    # copy all task list
+                            #print(f'[%d][%d][%d] = [%d][%d][%d]'% (x, i+1, w, x, i,w-tasks[i].storyPoint))
+                            tdp[x][i+1][w] = tdp[x][i][w]
+                    else:
+                        for x in range(0, numOfTasks):    # copy all tasl list
+                            #print(f'[%d][%d][%d] = [%d][%d][%d]'% (x, i+1, w, x, i,w-tasks[i].storyPoint))
+                            tdp[x][i+1][w] = tdp[x][i][w-tasks[i].storyPoint]
+                        for x in range(0, numOfTasks):
+                            if (tdp[x][i+1][w].taskId == 0):           # find empty slot
+                               tdp[x][i+1][w] = tasks[i]
+                               break
                 else:
                     dp[i+1][w] = dp[i][w]
+                    tdp[i+1][w] = tdp[i][w]         # copy list of tasks
+
+        for x in range (0, numOfTasks+1):
+            if(tdp[x][numOfTasks][w].taskId != 0):
+                print(f'taskID:%d, sp:%d val:%d'% (tdp[x][numOfTasks][w].taskId, tdp[x][numOfTasks][w].storyPoint, tdp[x][numOfTasks][w].value))
         return (dp[numOfTasks][w])
 #main ---------------
 Sprint1 = Scheduler()
 InputTasks = [Task(1001,2,3), Task(1002,1,2), Task(1003,3,6), Task(1004,2,1), Task(1005,1,3), Task(1006,5, 85)]
 
 totalPoint = Sprint1.scheduleMuxSPvalue(InputTasks, 9)
-print (totalPoint)
+print (f'maximum story point (%d)'% totalPoint)
